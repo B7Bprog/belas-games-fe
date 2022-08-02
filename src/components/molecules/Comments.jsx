0e6920464078e5a7895037ee02c3ca11d6441a43
Bela Bertalan <belabertalan@gmail.com>
2022-08-02 08:45:23 +0100

    Add delete comment function

--- src/components/molecules/Comments.jsx
+++ src/components/molecules/Comments.jsx
@@ -5,7 +5,10 @@ import styles from "./styles/Comments.module.css";
 
 const Comments = ({ review_id }) => {
   const [comments, setComments] = useState([]);
+  const [isDeleting, setIsDeleting] = useState(false);
   const { user } = useContext(userContext);
+
+  console.log(user, "<<<user");
   useEffect(() => {
     axios
       .get(
@@ -14,9 +17,25 @@ const Comments = ({ review_id }) => {
       .then((response) => {
         setComments(response.data.comments);
       });
-  }, [review_id]);
+  }, [review_id, isDeleting]);
 
-  return (
+  function handleDelete(e, comment_id) {
+    setIsDeleting(true);
+    axios
+      .delete(`https://belas-games.herokuapp.com/api/comments/${comment_id}`)
+      .then((response) => {
+        console.log(response, "response");
+        setIsDeleting(false);
+      });
+    //delete here
+    console.log("deleted comment");
+  }
+
+  console.log(comments[4], "comment no. 4");
+
+  return isDeleting ? (
+    <h2>Deleting comment</h2>
+  ) : (
     <div>
       <h3>Comments:</h3>
       <div>
@@ -30,6 +49,18 @@ const Comments = ({ review_id }) => {
               <h3 className={styles.commentCredentials}>
                 Posted on: {new Date(comment.created_at).toDateString()}
               </h3>
+              {user.username === comment.author ? (
+                <div>
+                  <button
+                    type="button"
+                    onClick={(e) => handleDelete(e, comment.comment_id)}
+                  >
+                    Delete comment
+                  </button>
+                </div>
+              ) : (
+                <div></div>
+              )}
             </div>
           );
         })}
